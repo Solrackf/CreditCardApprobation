e5dae64f577a21202d641ab07aef8663d3b3978b
Sebastian Diaz <edgarsebastian.pena.contractor@bbva.com>
2024-05-16 23:29:49 -0500

    validacion de usuarios y modelo de cards

---
+++ server/models/Cards.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const CardsSchema = new mongoose.Schema({
+  cedulaUsuario: {
+    type: Number,
+    required: true,
+    ref: "Users",
+  },
+  numero: {
+    type: String,
+    required: true,
+  },
+  fechaExpiracion: {
+    type: Date,
+    required: true,
+  },
+  franquicia: {
+    type: String,
+  },
+});
+
+module.exports = mongoose.model("Cards", CardsSchema);
--- server/repositories/UserRepository.js
+++ server/repositories/UserRepository.js
@@ -3,25 +3,44 @@ const Users = require("../models/Users");
 class UserRepository {
   async create(userData, res) {
     try {
-      const nuevoUser = new Users(userData);
-      await nuevoUser.save();
-      return nuevoUser;
+      if(_userIsValid(userData.salarioMensual)){
+        const nuevoUser = new Users(userData);
+        await nuevoUser.save();
+        return nuevoUser;
+      }else{
+        res.status(400).json({ error: "Usuario no apto para tarjeta de crédito" });
+      }
+
     } catch (error) {
-        let messageError = error.message
-        console.error('Error al insertar el usuario:', messageError);
-        res.status(500).json({ error: messageError });
+      let messageError = error.message;
+      console.error("Error al insertar el usuario:", messageError);
+      res.status(500).json({ error: messageError });
     }
   }
 
-  userIsValid(){
-    isValid = false;
+  _userIsValid(salarioMensual) {
+    let isValid = false;
+    const randomNumber = Math.random();
+
+    let baseProbability = 0.5;
+
+    if (salarioMensual > 2000000) {
+      baseProbability += 0.2; // Aumenta la probabilidad en un 20%
+    }
+
+    if (salarioMensual > 4000000) {
+      baseProbability += 0.3; // Aumenta la probabilidad en un 30%
+    }
+    
+
+    if (randomNumber < baseProbability) {
+      isValid = true;
+    }
 
     return isValid;
   }
 
+
 }
-
-
-
 
 module.exports = new UserRepository();
