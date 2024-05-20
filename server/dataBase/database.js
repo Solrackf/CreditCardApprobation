7a5b3309ddecc60aad991981be10ebf250338b3b
Solrackf <carlosescorcia81@gmail.com>
2024-05-19 22:35:32 -0500

    Solución bug de conexión a la base de datos

--- server/dataBase/database.js
+++ server/dataBase/database.js
@@ -1,15 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
+require('dotenv').config()
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    });
-    console.log('Conexión exitosa a la base de datos');
+    })
+    console.log('Conexión exitosa a la base de datos')
   } catch (error) {
-    console.error('Error al conectar a la base de datos:', error.message);
-    process.exit(1);
+    console.error('Error al conectar a la base de datos:', error.message)
+    process.exit(1)
   }
 };
 
